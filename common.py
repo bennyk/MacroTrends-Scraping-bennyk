d4a30f666a2f8c76800d52ff22ddb15be5cec832
Benny Khoo <benny.khoo@yahoo.com>
2023-11-19 15:57:50 +0800

    New structure in Clipboard

--- common.py
+++ common.py
@@ -10,8 +10,12 @@ from selenium.webdriver.common.keys import Keys
 import time
 import re
 import pandas as pd
+from openpyxl import Workbook, worksheet
+from openpyxl.chart import LineChart, Reference
+from openpyxl.styles import Font, Alignment
 
 from itertools import tee
+from typing import List, Dict
 
 
 def pairwise(iterable):
@@ -131,6 +135,15 @@ def curly_brace(my_str, open_list=None, close_list=None):
     return _check(my_str)
 
 
+class MyVar:
+    def __init__(self):
+        self.var = 1
+
+    def gen_var(self):
+        self.var += 1
+        return self.var
+
+
 class Clipboard:
     def __init__(self):
         self.wb = Workbook()
@@ -160,6 +173,95 @@ class Clipboard:
     def save(self):
         # Save the Excel file
         self.wb.save(self.excel_file)
+
+    def insert_newsheet(self, new_sheetname: str, col_tuples: List[Dict]):
+        ws = self.wb.create_sheet(new_sheetname)
+        new_sheet_index = len(self.wb.sheetnames)-1
+        self.wb.active = new_sheet_index
+
+        # Iterate to ~60 rows
+        # len 58
+        for i in range(1, 60):
+            ws.cell(column=1, row=i, value='=Income!{letter}{index}'.format(index=i, letter=column_to_letter(1)))
+
+        for t in col_tuples:
+            for i in range(1, 60):
+                ws.cell(column=t['idx'], row=i, value='={table}!{letter}{index}'
+                        .format(index=i, letter=column_to_letter(t['ref']), table=t['table']))
+        return ws
+
+    def insert_chart(self, ws, start, end, new_sheetname):
+
+        # TODO insert start result col and end result col
+        for j in range(1, end):
+            ws.column_dimensions[column_to_letter(j)].width = 10
+            ws.cell(column=j, row=1).alignment = Alignment(wrapText=True)
+
+        # Graph data
+        chart = LineChart()
+        for i in range(start, end):
+            letter = column_to_letter(i)
+            data = Reference(ws, range_string=f'{new_sheetname}!{letter}1:{letter}60')
+            chart.add_data(data, titles_from_data=True)
+
+        category = Reference(ws, range_string=f'{new_sheetname}!A2:A60')
+        chart.set_categories(category)
+        ws.add_chart(chart, 'D3')
+
+    def insert_debt(self):
+        var = MyVar()
+        cash_col = var.gen_var()
+        lt_assets_col = var.gen_var()
+        total_current_liab_col = var.gen_var()
+        lt_liabilities_col = var.gen_var()
+        equity_col = var.gen_var()
+        lt_liabilities_to_assets_col = var.gen_var()
+        net_debt_to_equity_col = var.gen_var()
+        cash_to_short_term_col = var.gen_var()
+
+        col_tuples = [
+            # Cash
+            {'ref': 2, 'idx': cash_col, 'text': 'LT asset', 'table': 'Balance'},
+            # LT assets
+            {'ref': 12, 'idx': lt_assets_col, 'text': 'LT asset', 'table': 'Balance'},
+            # ST debt
+            {'ref': 14, 'idx': total_current_liab_col, 'text': 'LT debt', 'table': 'Balance'},
+            # LT liabilities
+            {'ref': 17, 'idx': lt_liabilities_col, 'text': 'LT liabilities', 'table': 'Balance'},
+            # Shareholder equity
+            {'ref': 23, 'idx': equity_col, 'text': 'Equity', 'table': 'Balance'},
+        ]
+        new_sheetname = 'Debt'
+        ws = self.insert_newsheet(new_sheetname, col_tuples)
+
+        ws.cell(column=lt_liabilities_to_assets_col, row=1, value='Long term liabilities to assets')
+        ws.cell(column=net_debt_to_equity_col, row=1, value='Net debt to equity')
+        ws.cell(column=cash_to_short_term_col, row=1, value='Cash to short-term borrowing')
+        for i in range(2, 60):
+            ws.cell(column=lt_liabilities_to_assets_col, row=i, value='=${lt_liabilities}{index}/${lt_assets}{index}'
+                    .format(index=i,
+                            lt_liabilities=column_to_letter(lt_liabilities_col),
+                            lt_assets=column_to_letter(lt_assets_col)))
+            ws['{}{}'.format(column_to_letter(lt_liabilities_to_assets_col), i)].number_format = '0%'
+
+            ws.cell(column=net_debt_to_equity_col, row=i,
+                    value='=(${total_lt_liab}{index}+${total_current_liab}{index}-${cash}{index})/${equity}{index}'
+                    .format(index=i,
+                            cash=column_to_letter(cash_col),
+                            total_current_liab=column_to_letter(total_current_liab_col),
+                            total_lt_liab=column_to_letter(lt_liabilities_col),
+                            equity=column_to_letter(equity_col), ))
+            ws['{}{}'.format(column_to_letter(net_debt_to_equity_col), i)].number_format = '0%'
+
+            ws.cell(column=cash_to_short_term_col, row=i,
+                    value='=${cash}{index}/${total_current_liab}{index}'
+                    .format(index=i,
+                            cash=column_to_letter(cash_col),
+                            total_current_liab=column_to_letter(total_current_liab_col), ))
+            ws['{}{}'.format(column_to_letter(cash_to_short_term_col), i)].number_format = '0.00'
+
+        self.insert_chart(ws, lt_liabilities_to_assets_col, cash_to_short_term_col + 1,
+                          new_sheetname)
 
 
 def old_run_main():
